74f1d35df440c1a40a26bea709e577a36f167887
Pivick T. Lorenzo <pivicklorenzo9@gmail.com>
2026-09-13 13:04:04 +0800

    update cors

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,10 @@ import routes from './routes'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  credentials: true
+}))
 
 
 app.use(express.json({ limit: '50mb' }));
